c4c98d686d97039ba084008829ec1804b58c7d6e
mayang <mayang@souche.com>
2021-05-07 19:40:25 +0800

    后台学生信息curd完成

--- provider/src/main/java/com/mayang/provider/ProviderApplication.java
+++ provider/src/main/java/com/mayang/provider/ProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubbo
-@MapperScan(basePackages = "com.kaven.mybatisplus.dao")
+@MapperScan(basePackages = "com.mayang.provider.dao.mapper")
 public class ProviderApplication {
 
     public static void main(String[] args) {
